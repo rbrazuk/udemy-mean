7c85ac7c5abd4b743ac97bc22547fe786934e781
Ross Brazuk <ross.brazuk@gmail.com>
2017-06-19 16:55:23 -0400

    section 22 - querying mongodb from application

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,8 +1,12 @@
+var dbconn = require('../data/dbconnection.js');
+var ObjectId = require('mongodb').ObjectId;
 var hotelData = require('../data/hotel-data.json');
 
+
 module.exports.hotelsGetAll = function(req,res) {
-    console.log("GET all hotels");
-    console.log(req.query);
+    
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
     
     var offset = 0;
     var count = 5;
@@ -15,20 +19,37 @@ module.exports.hotelsGetAll = function(req,res) {
         count = parseInt(req.query.count, 10);
     }
     
-    var returnData = hotelData.slice(offset, offset + count);
+    collection
+        .find()
+        .skip(offset)
+        .limit(count)
+        .toArray(function(err, docs) {
+            console.log("Found hotels", docs);
+            res
+                .status(200)
+                .json(docs);
+        });
     
-    res
-        .status(200)
-        .json(returnData);
+    
+    
+
 };
 
 module.exports.hotelsGetOne = function(req,res) {
+    var db = dbconn.get();
+    var collection = db.collection('hotels');
+    
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId];
+    
     console.log("GET hotelId", hotelId);
-    res
-        .status(200)
-        .json(thisHotel);
+    
+    collection 
+        .findOne({ _id : ObjectId(hotelId) }, function(err, doc) {
+        res
+            .status(200)
+            .json(doc);
+    });
+    
 };
 
 module.exports.hotelsAddOne = function(req, res) {
